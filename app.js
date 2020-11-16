1eb5218c1ef5546f253d190f24c71c862fdc4c35
ReneSchmitz <post.fuer.rene.schmitz@web.de>
2020-11-16 13:29:00 +0100

    Implement delete

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { readCommandLineArguments } = require("./lib/commandLine");
 const { connect, close } = require("./lib/database");
 const { getPassword, setPassword } = require("./lib/passwords");
@@ -5,32 +6,67 @@ const { askForMasterPassword } = require("./lib/questions");
 const { isMasterPasswordCorrect } = require("./lib/validation");
 
 async function run() {
-  console.log("Connecting to database...");
-  await connect(
-    "mongodb+srv://Rene:vWsv4VcyWszk5KYo@cluster0.oloe7.mongodb.net/pw4u?retryWrites=true&w=majority",
-    "pw4u"
-  );
-  console.log("Connected to database 🎉");
+  try {
+    console.log("Connecting to database...");
+    await connect(process.env.MONGO_DB_URI, process.env.MONGO_DB_NAME);
+    console.log("Connected to database 🎉");
+    const masterPassword = await askForMasterPassword();
 
-  const masterPassword = await askForMasterPassword();
+    if (!(await isMasterPasswordCorrect(masterPassword))) {
+      console.error("You are not welcome here! 👿 Try again!");
+      return run();
+    }
 
-  if (!isMasterPasswordCorrect(masterPassword)) {
-    console.error("You are not welcome here! 👿 Try again!");
-    return run();
+    const [passwordName, newPasswordValue] = readCommandLineArguments();
+    if (!passwordName) {
+      console.error("Missing password name!");
+      return process.exit(9);
+    }
+
+    if (newPasswordValue) {
+      await setPassword(passwordName, newPasswordValue);
+      console.log(`Password ${passwordName} set 🎉`);
+    } else {
+      const passwordValue = await getPassword(passwordName);
+      console.log(`Your password is ${passwordValue} 🎉`);
+    }
+  } catch (error) {
+    if (error.message.match("E11000")) {
+      console.error("DUPLICATE KEY JO!");
+    } else {
+      console.error(error);
+    }
+  } finally {
+    await close();
   }
-  const [passwordName, newPasswordValue] = readCommandLineArguments();
-  if (!passwordName) {
-    console.error("Missing password name!");
-    return process.exit(9);
-  }
-  if (newPasswordValue) {
-    await setPassword(passwordName, newPasswordValue);
-    console.log(`Password ${passwordName} set 🎉`);
-  } else {
-    const passwordValue = await getPassword(passwordName);
-    console.log(`Your password is ${passwordValue} 🎉`);
-  }
-  close();
 }
+
+//   console.log("Connecting to database...");
+//   await connect(
+//     "mongodb+srv://Rene:vWsv4VcyWszk5KYo@cluster0.oloe7.mongodb.net/pw4u?retryWrites=true&w=majority",
+//     "pw4u"
+//   );
+//   console.log("Connected to database 🎉");
+
+//   const masterPassword = await askForMasterPassword();
+
+//   if (!isMasterPasswordCorrect(masterPassword)) {
+//     console.error("You are not welcome here! 👿 Try again!");
+//     return run();
+//   }
+//   const [passwordName, newPasswordValue] = readCommandLineArguments();
+//   if (!passwordName) {
+//     console.error("Missing password name!");
+//     return process.exit(9);
+//   }
+//   if (newPasswordValue) {
+//     await setPassword(passwordName, newPasswordValue);
+//     console.log(`Password ${passwordName} set 🎉`);
+//   } else {
+//     const passwordValue = await getPassword(passwordName);
+//     console.log(`Your password is ${passwordValue} 🎉`);
+//   }
+//   close();
+// }
 
 run();
